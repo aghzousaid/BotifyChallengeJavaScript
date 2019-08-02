0dca03408ebece0601cfa4180eb9853a2439c436
aghzou said <32544746+aghzousaid@users.noreply.github.com>
2019-08-02 18:29:07 +0200

    Step 2 : Remote Data Fetching

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,73 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
-import logo from './logo.svg';
+import Chart from 'react-google-charts';
+import Component from 'react-component-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RemoteData extends React.Component {
+  render() {
+    return (
+      <Component
+        initialState={{ dataLoadingStatus: 'loading', chartData: [] }}
+        didMount={async function(component) {
+          try {
+            //Get data of NASA API
+            const response = await fetch(
+              'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY',
+            )
+            if (!response.ok) {
+              throw Error(response.statusText)
+            }
+            const json = await response.json()
+  
+            const chartData = [['NEO Name', 'Min Estimated Diameter (KM)', 'Max Estimated Diameter (KM)']]
+            for (let i = 0; i < json.page.size; i += 1) {
+              chartData.push([json.near_earth_objects[i].name, 
+                json.near_earth_objects[i].estimated_diameter.kilometers.estimated_diameter_min, 
+                json.near_earth_objects[i].estimated_diameter.kilometers.estimated_diameter_max
+                ])
+            } 
+          
+              // Sorting Data By descending Order
+              chartData.sort(function(a,b) {
+                return b[1]-a[1]
+              });
+              
+              component.setState({
+                dataLoadingStatus: 'ready',
+                sortBy: ['Min Estimated Diameter (KM)'],
+                chartData: chartData
+              })
+          }catch (error) {
+            console.log(error)
+          }
+    
+        }}
+      >
+        {component => {
+          return component.state.dataLoadingStatus === 'ready' ? (
+            <Chart
+              chartType='BarChart'
+              data={component.state.chartData}
+              options={{
+                chartArea: { width: '50%' },
+                hAxis: {
+                  title: 'Min Estimated Diameter (KM)',
+                  minValue: 0
+                },
+                vAxis: {
+                  title: 'Neo Name',
+                },
+              }}
+              
+              rootProps={{ 'data-testid': '1' }}
+            />
+          ) : (
+            <div>Fetching data from API</div>
+          )
+        }}
+      </Component>
+    );
+  }
 }
 
-export default App;
-
+export default RemoteData;
